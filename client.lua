a4f41eb4791d7269f2a17253b0fce83cf1094d35
sati5M <70115567+sati5M@users.noreply.github.com>
2023-01-03 12:11:21 +0000

    update: Shorten some lines to one, cancel hack and remove item if hack is failed

--- client.lua
+++ client.lua
@@ -1,27 +1,9 @@
 QBCore = exports['qb-core']:GetCoreObject()
-local atmProps = {
-    "prop_atm_01",
-    "prop_atm_02",
-    "prop_atm_03",
-    "prop_fleeca_atm"
-}
-
-
-exports['qb-target']:AddTargetModel(atmProps, { -- This defines the models, can be a string or a table
-options = { -- This is your options table, in this table all the options will be specified for the target to accept
-    { -- This is the first table with options, you can make as many options inside the options table as you want
-    num = 1, -- This is the position number of your option in the list of options in the qb-target context menu (OPTIONAL)
-    icon = 'fas fa-example', -- This is the icon that will display next to this trigger option
-    label = 'Rob ATM', -- This is the label of this option which you would be able to click on to trigger everything, this has to be a string
-
-    action = function(entity) -- This is the action it has to perform, this REPLACES the event and this is OPTIONAL
+local atmProps = {"prop_atm_01","prop_atm_02","prop_atm_03","prop_fleeca_atm"}
+exports['qb-target']:AddTargetModel(atmProps, {options = { { icon = 'fas fa-example', label = 'Rob ATM', item = "laptop",
+    action = function(entity)
         StartRobbery(entity)
-    end,
-    }
-},
-distance = 2.5, -- This is the distance for you to be at for the target to turn blue, this is in GTA units and has to be a float value
-})
-
+    end,}},distance = 2.5,})
 AddEventHandler('onResourceStop', function(resource)
    if resource == GetCurrentResourceName() then
         exports['qb-target']:RemoveTargetModel(atmProps, 'Rob ATM')
@@ -34,21 +16,15 @@ function StartRobbery(entity)
         if canRob then
             exports['ps-ui']:Scrambler(function(success)
                 if success then
-                    QBCore.Functions.Progressbar('Hacking ATM', 'Hacking ATMs', 5000, false, true, {
-                        disableMovement = true,
-                        disableCarMovement = true,
-                        disableMouse = false,
-                        disableCombat = true,
-                    }, {
-                        animDict = 'anim@gangops@facility@servers@',
-                        anim = 'hotwire',
-                        flags = 0
-                    }, {}, {}, function()
+                    QBCore.Functions.Progressbar('Hacking ATM', 'Hacking ATMs', 5000, false, true, {disableMovement = true,disableCarMovement = true,disableMouse = false,disableCombat = true,}, {animDict = 'anim@gangops@facility@servers@',anim = 'hotwire',flags = 0}, {}, {}, 
+                    function()
                         ClearPedTasksImmediately(ped)
                         TriggerServerEvent("SCHackATMs:HackComplete", objectCoords)
                     end, function() 
                         TriggerServerEvent("SCHackATMS:CancelHack", objectCoords)
                     end)
+                else
+                    TriggerServerEvent("SCHackATMS:CancelHack", objectCoords, true)
                 end
             end, "numeric", 30, 0)
         end
--- server.lua
+++ server.lua
@@ -1,11 +1,6 @@
 QBCore = exports['qb-core']:GetCoreObject()
-local inHack = {}
-local hackedATMs = {}
-local itemsToGive = {
-    {["lockpick"] = 3, ["laptop"] = 2},
-    {["lockpick"] = 1, ["laptop"] = 4},
-}
-
+local hackedATMs, inHack = {}, {}
+local itemsToGive = {{["lockpick"] = 3, ["laptop"] = 2}, {["lockpick"] = 1, ["laptop"] = 4},}
 RegisterNetEvent("SCHackATMs:HackComplete", function(objCoords)
     local source = source
     if inHack[source] == objCoords then
@@ -19,31 +14,29 @@ RegisterNetEvent("SCHackATMs:HackComplete", function(objCoords)
         end
         inHack[source] = false
         QBCore.Functions.Notify(source, 'ATM robbed.', 'success', 7500)
-
     end
 end)
-
-RegisterNetEvent("SCHackATMS:CancelHack", function(objCoords)
+RegisterNetEvent("SCHackATMS:CancelHack", function(objCoords, fail)
     local source = source
     if inHack[source] == objCoords then
         inHack[source] = false
-        QBCore.Functions.Notify(source, 'Hack cancelled.', 'error', 7500)
-    end
-end) 
-
-QBCore.Functions.CreateCallback('SCHackATMS:ATMCheck', function(source, cb, coords)
-    local Player = QBCore.Functions.GetPlayer(source)
-    local item = Player.Functions.GetItemByName("laptop")
-    if item ~= nil then
-        if item.amount > 0 then
-            if not hackedATMs[coords] then
-                inHack[source] = coords
-                hackedATMs[coords] = true
-                cb(true)
-            else
-                QBCore.Functions.Notify(source, 'This ATM has already been robbed!', 'error', 7500)
-            end
+        hackedATMs[objCoords] = false
+        if not fail then
+            QBCore.Functions.Notify(source, 'Hack cancelled.', 'error', 7500)
+        else
+            QBCore.Functions.Notify(source, 'You failed the hack', 'error', 7500)
         end
     end
-    cb(false)
+end) 
+QBCore.Functions.CreateCallback('SCHackATMS:ATMCheck', function(source, cb, coords)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if not hackedATMs[coords] then
+        inHack[source] = coords
+        hackedATMs[coords] = true
+        Player.Functions.RemoveItem("laptop")
+        cb(true)
+    else
+        QBCore.Functions.Notify(source, 'This ATM has already been robbed!', 'error', 7500)
+        cb(false)
+    end
 end)
